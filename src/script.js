c9ec23021c46883aec4f3ee71e4c0b779c86652b
Fedorov Ivan <sirarthurjohnevans@live.com>
2021-04-26 10:41:04 +0300

    Add action for calculation button

---
+++ src/script.js
@@ -0,0 +1,29 @@
+const billInput = document.querySelector('.bill-input');
+const serviceInput = document.querySelector('.service-quality');
+const guestNumberInput = document.querySelector('.quest-number');
+
+const actionButtonCalc = document.querySelector('.action-calc');
+
+const personBill = (bill, guestNumber) => (Math.round((bill * 10) / guestNumber)) / 10;
+
+const calcTips = (billP, tipsPercent) => (Math.round(billP * tipsPercent * 10)) / 10;
+
+const calcSum = (billForPerson, tips) => billForPerson + tips;
+
+const tipP = document.querySelector('.tip-value');
+const sumP = document.querySelector('.sum-value');
+
+const calcClickHandler = () => {
+  const bill = parseInt(billInput.value, 10);
+  const percentOfTip = parseInt(serviceInput.value, 10) / 100;
+  const guestNumber = parseInt(guestNumberInput.value, 10);
+
+  const billForPerson = personBill(bill, guestNumber);
+  const tipForPerson = calcTips(billForPerson, percentOfTip);
+  const sumForPerson = calcSum(billForPerson, tipForPerson);
+
+  tipP.innerText = `${tipForPerson} ₽`;
+  sumP.innerText = `${sumForPerson} ₽`;
+};
+
+actionButtonCalc.addEventListener('click', calcClickHandler);
